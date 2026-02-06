58fc2820aec3b58fe3ae2ae5d075de1032594ca8
ITI Tehu <ititehu@gmail.com>
2026-02-06 10:01:08 +0000

    in add member in joining date i m not able to select any date other than

--- src/components/members/add-member-form.tsx
+++ src/components/members/add-member-form.tsx
@@ -244,7 +244,7 @@ export default function AddMemberForm({ setDialogOpen }: AddMemberFormProps) {
                             selected={field.value}
                             onSelect={field.onChange}
                             disabled={(date) =>
-                                date > new Date() || date < new Date("1900-01-01")
+                                date < new Date("1900-01-01")
                             }
                             initialFocus
                         />

b1e04af554a5a9f3c2050669dda59b506748c68d
ITI Tehu <ititehu@gmail.com>
2026-02-06 06:21:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1383481872-e49ce",
+  "appId": "1:954394104468:web:cb90d5b0e3d61f22609a81",
+  "apiKey": "AIzaSyAEkUoBP-yL70oRf8qOGyR9HZsqaeZbRBQ",
+  "authDomain": "studio-1383481872-e49ce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "954394104468"
+};
